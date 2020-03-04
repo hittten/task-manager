a122eff19215a8f02474de93b2079981a7302676
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2020-03-04 21:04:20 +0100

    list tasks

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log('hello');
+import {Task} from "./task";
+import {TASKS} from "./mock-task";
+
+// Elements
+const taskListElement: HTMLUListElement = document.querySelector('#taskList');
+
+// Data Functions
+function listTasks(): Task[] {
+  return TASKS;
+}
+
+// HTML Functions
+function listTasksElements(element: HTMLUListElement, tasks: Task[]) {
+  let html = '';
+
+  tasks.forEach(task => html += `
+    <li>
+      <div>
+        <input type="checkbox" ${task.done ? 'checked' : ''}>
+        <span>${task.description}</span>
+      </div>
+      <i class="material-icons btn-delete">delete_outline</i>
+    </li>
+  `)
+
+  element.innerHTML = html;
+}
+
+// Events
+
+// Load
+listTasksElements(taskListElement, listTasks());
---
+++ src/mock-task.ts
@@ -0,0 +1,39 @@
+import {Task} from "./task";
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    description: 'Esto es una tarea sin hacer',
+    done: false,
+  },
+  {
+    id: 2,
+    description: 'Esto es una tarea hecha',
+    done: true,
+  },
+  {
+    id: 3,
+    description: 'Esto es una tarea de al menos dos lineas, no se que mas poner',
+    done: false,
+  },
+  {
+    id: 4,
+    description: 'Una tarea en modo edición',
+    done: false,
+  },
+  {
+    id: 5,
+    description: 'Una tarea para eliminar',
+    done: true,
+  },
+  {
+    id: 6,
+    description: 'otra tarea',
+    done: false,
+  },
+  {
+    id: 7,
+    description: 'otra tarea',
+    done: false,
+  },
+];
