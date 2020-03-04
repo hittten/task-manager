d5cf311db9048d3979adfcdd31490436433971b3
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2020-03-04 21:18:09 +0100

    create tasks

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,37 @@ import {TASKS} from "./mock-task";
 
 // Elements
 const taskListElement: HTMLUListElement = document.querySelector('#taskList');
+const taskInputElement: HTMLInputElement = document.querySelector('#taskInput');
 
 // Data Functions
 function listTasks(): Task[] {
   return TASKS;
 }
 
+function createTask(description: string): Task {
+  const tasks = TASKS;
+  const newTask: Task = {
+    id: tasks.length + 1,
+    description: description,
+    done: false,
+  };
+
+  tasks.push(newTask);
+
+  return newTask;
+}
+
 // HTML Functions
 function listTasksElements(element: HTMLUListElement, tasks: Task[]) {
   let html = '';
 
-  tasks.forEach(task => html += `
+  tasks.forEach(task => html += createTaskElement(task));
+
+  element.innerHTML = html;
+}
+
+function createTaskElement(task: Task): string {
+  return `
     <li>
       <div>
         <input type="checkbox" ${task.done ? 'checked' : ''}>
@@ -21,12 +41,21 @@ function listTasksElements(element: HTMLUListElement, tasks: Task[]) {
       </div>
       <i class="material-icons btn-delete">delete_outline</i>
     </li>
-  `)
-
-  element.innerHTML = html;
+  `
 }
 
 // Events
+taskInputElement.onkeyup = (e) => {
+  const input = e.target as HTMLInputElement;
+  if (e.key === 'Enter' && input.value) {
+    input.focus();
+    const task = createTask(input.value);
+    const taskElement = createTaskElement(task);
+    input.value = '';
+
+    taskListElement.innerHTML += taskElement;
+  }
+};
 
 // Load
 listTasksElements(taskListElement, listTasks());
